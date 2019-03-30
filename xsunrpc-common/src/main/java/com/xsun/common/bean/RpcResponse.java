92ba8541f9164b7cd0897104dd74e9464c53b24d
dynamic-sevn <nuaa_sunhr@yeah.net>
2019-03-31 00:52:23 +0800

    Add server handler

--- xsunrpc-common/src/main/java/com/xsun/common/bean/RpcResponse.java
+++ xsunrpc-common/src/main/java/com/xsun/common/bean/RpcResponse.java
@@ -1,9 +1,15 @@
 package com.xsun.common.bean;
 
+import lombok.Data;
+
 /**
  * created at 20:40, 2019/3/23
  *
  * @author sunhaoran <nuaa_sunhr@yeah.net>
  */
+@Data
 public class RpcResponse {
+    private Object result ;
+
+    private Exception exception ;
 }
--- xsunrpc-server/src/main/java/com/xsun/rpc/server/RpcServerHandler.java
+++ xsunrpc-server/src/main/java/com/xsun/rpc/server/RpcServerHandler.java
@@ -1,9 +1,13 @@
 package com.xsun.rpc.server;
 
 import com.xsun.common.bean.RpcRequest;
+import com.xsun.common.bean.RpcResponse;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.apache.commons.lang3.StringUtils;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -20,7 +24,28 @@ public class RpcServerHandler extends SimpleChannelInboundHandler<RpcRequest> {
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, RpcRequest msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, RpcRequest request) throws Exception {
+        String serviceName = request.getInterfaceName() ;
+        if(StringUtils.isNotBlank(request.getServiceVersion())){
+            serviceName += "-" + request.getServiceVersion() ;
+        }
+        Object serviceObj = nameServiceMap.get(serviceName) ;
+        if(serviceObj == null){
+            throw new RuntimeException(String.format("Can not find the service %s ", serviceName)) ;
+        }
+        RpcResponse rpcResponse = new RpcResponse();
+        try {
+            Class<?> clazz = serviceObj.getClass();
+            Class<?>[] parameterTypes = request.getParameterTypes();
+            Object[] parameters = request.getParameters();
+            Method method = clazz.getMethod(request.getMethodName(), parameterTypes);
+            Object result = method.invoke(serviceObj, parameters);
+            rpcResponse.setResult(result);
+        }catch (Exception e){
+            rpcResponse.setException(e);
+        }
+
+        ctx.writeAndFlush(rpcResponse).addListener(ChannelFutureListener.CLOSE) ;
 
     }
 }
