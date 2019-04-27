0435d2ab76aeed14f08f5c139e7d821e7d81634c
dynamic-sevn <nuaa_sunhr@yeah.net>
2019-04-27 23:59:21 +0800

    server demo

---
+++ xsunrpc-demo-api/src/main/java/com/xsun/rpc/demo/api/HelloWorldService.java
@@ -0,0 +1,10 @@
+package com.xsun.rpc.demo.api;
+
+/**
+ * created at 20:40, 2019/3/23
+ *
+ * @author sunhaoran <nuaa_sunhr@yeah.net>
+ */
+public interface HelloWorldService {
+    String hello(String name);
+}
---
+++ xsunrpc-demo-server/src/main/java/com/xsun/rpc/demo/server/impl/HelloWorldImpl.java
@@ -0,0 +1,17 @@
+package com.xsun.rpc.demo.server.impl;
+
+import com.xsun.rpc.demo.api.HelloWorldService;
+import com.xsun.rpc.server.XsunRpc;
+
+/**
+ * created at 20:40, 2019/3/23
+ *
+ * @author sunhaoran <nuaa_sunhr@yeah.net>
+ */
+@XsunRpc(HelloWorldService.class)
+public class HelloWorldImpl implements HelloWorldService {
+    @Override
+    public String hello(String name) {
+        return name + "hello world !";
+    }
+}
