d19fbc38484799d801524488bc18c9beb1ed9f23
dynamic-sevn <nuaa_sunhr@yeah.net>
2019-05-10 11:46:53 +0800

    fix address

---
+++ xsunrpc-demo-server/src/test/java/com/xsun/rpc/demo/service/test/AbstractTest.java
@@ -0,0 +1,15 @@
+package com.xsun.rpc.demo.service.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * created at 10:51, 2019-05-10
+ *
+ * @author sunhaoran
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:/context.xml"})
+public class AbstractTest {
+}
---
+++ xsunrpc-demo-server/src/test/java/com/xsun/rpc/demo/service/test/StartServer.java
@@ -0,0 +1,15 @@
+package com.xsun.rpc.demo.service.test;
+
+import org.junit.Test;
+
+/**
+ * created at 10:53, 2019-05-10
+ *
+ * @author sunhaoran
+ */
+public class StartServer extends AbstractTest{
+    @Test
+    public void start(){
+        System.out.println("start server");
+    }
+}
